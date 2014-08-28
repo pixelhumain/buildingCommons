dbe2de5c79cbca4af9b211bb323089efc9043e6b
Tibor Katelbach <oceatoon@gmail.com>
2014-08-28 20:06:34 +0400

    event microformat

---
+++ events/applications.js
@@ -0,0 +1,10 @@
+[
+{
+    "_id" : { "$oid" : "5374bbb32d53086b715ec714" },
+    "name" : "AzotLive",
+    "key" : "azotlive",
+    "registration" : "mailValidation",
+    "adminEmail" : "oceatoon@gmail.com",
+    "logoUrl":"http://qa.pixelhumain.com/ph/assets/75fc16b3/images/logo.png"
+}
+]
